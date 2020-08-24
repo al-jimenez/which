a150546d4ce0015d9d3a6c5cbfca0a75e21d1c23
Al Jimenez <al.jimenez@mail.com>
2020-08-24 11:51:25 -0500

    which.sh;

--- which.sh
+++ which.sh
@@ -1,37 +1,45 @@
 #!/usr/bin/env bash
-
-alias .whereis='/usr/bin/whereis'     # maintain command line access to default whereis
-
-# 📌 whereis() - macro of whereis.  find a program or a file and return path
-#    Usage:  whereis <progrem>, whereis <filename>
-whereis() {
-  [[ -z ${1}            ]] && { shift; ${FUNCNAME[0]} -?; return; }             # if 1st param is empty
-  #[[ "${1}" == "simple" ]] || [[ "${1}" == "-"  ]] && { shift; *; return; }
-  #[[ "${1}" == "pause"  ]] || [[ "${1}" == "p"  ]] && { shift; *; pause; return; }
-  [[ "${1}" == "?"      ]] || [[ "${1}" == "-?" ]] || [[ "${1}" == "help" ]] || [[ "${1}" == "-h" ]] && {   # ${FUNCNAME[0]} -?
-    echo -e "
-    PURPOSE: whereis is a bash function that overloads whereis. It locates programs
-    (using native 'whereis') or locates a file (i.e. script, etc).
-    (NOTE: uses 'find' which excludes ~\Library, searches current folder structure)
-
-    ${FUNCNAME[0]} <options> | <program name> | <file name>
+# 📌 which() - checks for alias, then checks for funciton, then checks /usr/bin/which
+#    Usage:  which <alias_name>, which <function_name>, which <progrem>, whcih -ff <file>
+alias .which='/usr/bin/which'  # default which
+which() {
+  _self="${FUNCNAME[0]}"
+ [[ -z ${1}               ]] && { shift; ${FUNCNAME[0]} -?; return; }
+ [[ "${1}" == "aliases"   ]] || [[ "${1}" == "-a"  ]] && { shift; alias | less; return; }
+ [[ "${1}" == "functions" ]] || [[ "${1}" == "-f"  ]] && { shift; declare -f | less; return; }
+ [[ "${1}" == "find"      ]] || [[ "${1}" == "-ff" ]] && { shift; find ~ -path ~/Library -prune -false -o -name ${1}; return; }
+ [[ "${1}" == "favs"      ]] || [[ "${1}" == "-v"  ]] && { shift; favs; return; }
+ [[ "${1}" == "help"      ]] || [[ "${1}" == "-h"  ]] || [[ "${1}" == "-?" ]] || [[ "${1}" == "?" ]] && {
+ echo -e "
+    PURPOSE: which is a bash function that overloads which. '${FUNCNAME[0]}()' looks for aliases/functions,
+    then programs (using native 'which')";
+ #_self="$(alias | grep 'alias' | grep "${FUNCNAME[0]}" | awk '/alias/ { print $2 }' | cut -f 1 -d '=')";
+ [[ "${_self}" != "${FUNCNAME[0]}" ]] && { echo -e "\n\t  (NOTE: '${_self}' is now an alias for this tool: $(alias ${_self}))"; }
+ echo -e "
+    ${FUNCNAME[0]} <options> | <alias name> | <function name> | <program name>
 
     OPTIONS:
-     -? | help     Display this usage help message.
+      -a  | aliases   Display all aliases with 'less' 
+      -f  | functions Display all fucntions with 'less'
+      -ff | find      find a specifc file and return path
+      -v  | favs      Favorite aliases, functions, and scripts (file: .favs)
+      -?  | help      This help
 
     USAGE:
-     ${FUNCNAME[0]} which      - returns '/usr/bin/which'
-     ${FUNCNAME[0]} whereis    - returns '/usr/bin/whereis'
-     ${FUNCNAME[0]} dotfile.sh - returns locations of dotfiles.sh (w/find)
-     ${FUNCNAME[0]} chknet.sh  - returns locations of chknet.sh (w/find)
-    "
-    return;
-    }
-    [[ "${1}" != "" ]] && {
-       /usr/bin/which ${@};
-       [[ $? != 0 ]] && { find ~ -path ~/Library -prune -false -o -name ${1}; return; }
-       [[ $? == 0 ]] && { /usr/bin/whereis ${@}; return; }
-       return;
-    }                                  # if 1st param is not empty and unique (not above)
- }
- export -f whereis;
+       ${FUNCNAME[0]}                 Display this help.
+       ${FUNCNAME[0]} <alias name>    Returns the alias definition.
+       ${FUNCNAME[0]} <function name> Returns the fucntion definition using 'less'
+       ${FUNCNAME[0]} <program>       Returns program using '/usr/bin/which'
+       ${FUNCNAME[0]} -a              Display all aliases with 'less'
+       ${FUNCNAME[0]} -f              Display all fucntions with 'less'
+       ${FUNCNAME[0]} -ff <file>      find a specifc file and return path
+       ${FUNCNAME[0]} -v              List (.favs) favorite aliases, functions, & scripts
+                              (NOTE: favs() maintains .favs file)
+   "
+   return;
+   }
+   [[ $( alias | grep "alias ${1}=" ) ]] && { alias ${1}; }  #return;
+   [[ $( declare -f ${1} ) ]] && { declare -f ${1} | less; } #return;
+   /usr/bin/which ${1};
+}
+export -f which;
